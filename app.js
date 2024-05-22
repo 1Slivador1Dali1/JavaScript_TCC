71f28b51ab4c7f71683bd215a559bde4691a72ef
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-22 21:18:33 +0300

    Ex 008

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // Ex 007
 
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+// let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
+
+// Ex 008
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
