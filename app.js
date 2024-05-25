5abadecc8e77834d605eaecbd8d4f056e5ba4a53
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-25 11:34:11 +0300

    Ex 009

--- app.js
+++ app.js
@@ -7,11 +7,31 @@
 
 // Ex 008
 
-console.log('Jonas');
-console.log(23);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Jonas';
+// let firstName = 'Jonas';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// Ex 009
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
