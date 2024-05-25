1d1f1763da127ea84ba66ddd3d9217f473e5caab
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-05-25 11:40:17 +0300

    Ex 010

--- app.js
+++ app.js
@@ -18,20 +18,33 @@
 
 // Ex 009
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// Ex 010
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990; // нельзя преопределять const
+
+// const job; // const нужно инициализирровать с самого начала при объявлении
+
+var job = 'prorammer'; // var плохая практика так как устарел и может запутать
+job = 'teacher';
