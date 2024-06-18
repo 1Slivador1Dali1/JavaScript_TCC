0fc801f15419a0279ca9aa350adf50041be19503
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-06-18 23:24:16 +0300

    Ex 047 - coding challenge #8

--- app.js
+++ app.js
@@ -656,22 +656,49 @@
 
 // Ex 046
 
-let rep = 1;
+// let rep = 1;
 
-while (rep <= 10) {
-    console.log(`Повторение номер ${rep}`);
-    rep++;
-}
+// while (rep <= 10) {
+//     console.log(`Повторение номер ${rep}`);
+//     rep++;
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
-while (dice !== 6) {
-    console.log(dice);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) console.log(`Число равно 6 цикл остановлен`)
-}
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+// while (dice !== 6) {
+//     console.log(dice);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) console.log(`Число равно 6 цикл остановлен`)
+// }
 
 // Ex 047 - coding challenge #8
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage([30, 203, 323]));
+console.log(calcAverage(totals));
 
 // Ex 048
 
